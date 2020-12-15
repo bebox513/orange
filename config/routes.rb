27de1766dc2a5cf0754a4fbba59d509d46945f6d
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2020-12-15 15:28:21 +0900

    チャット機能のルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
   #削除予定
   root to: 'tops#show'
-  
-  devise_for :corp_users, controllers: {
-    sessions:      'corp_users/sessions',
-    passwords:     'corp_users/passwords',
-    registrations: 'corp_users/registrations'
-  }
-  devise_for :users, controllers: {
-    sessions:      'users/sessions',
-    passwords:     'users/passwords',
-    registrations: 'users/registrations'
-  }
-  resources :user, :only => [:show]
+
+  devise_for :users
+
+  resources :users, :only => [:show]
+  resources :messages, :only => [:create]
+  resources :rooms, :only => [:new, :create, :show, :index]
 end
