92aac975fe040709442fcd045349089c72d558dc
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2020-12-16 09:32:01 +0900

    ユーザー設計の変更

--- spec/factories/corp_users.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :corp_user do
-    name { "山田太郎" }
-    sequence(:email) { |n| "example#{n}@example.com" }
-    password { "password" }
-    password_confirmation { 'password' }
-  end
-end
