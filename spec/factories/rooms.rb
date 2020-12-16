d2f7a7f44f344d7a307176ec9d6cb59b4110c936
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2020-12-16 09:27:57 +0900

    ルームのテストを記述

---
+++ spec/factories/rooms.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :room do
+    name { "山田太郎" }
+  end
+end
---
+++ spec/models/room_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Room, type: :model do
+  context "バリデーション" do
+    it "ルームネームがあれば有効な状態であること" do
+      room = create(:room)
+      expect(room).to be_valid
+    end
+    it "ルームネームがなければ無効な状態であること" do
+      room = build(:room, name: nil)
+      room.valid?
+      expect(room.errors[:name]).to include("を入力してください")
+    end
+    it "ルームネームが10文字以内であること" do
+      room = build(:room, name: "a" * 10)
+      expect(room).to be_valid
+    end
+    it "ルームネームが11文字以上の場合無効であること" do
+      room = build(:room, name: "a" * 11)
+      room.valid?
+      expect(room.errors[:name]).to include("は10文字以内で入力してください")
+    end
+  end
+end
