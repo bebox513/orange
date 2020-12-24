3a8efc1377c7ee7cac9f05128d2f807d5019be82
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2020-12-24 15:04:32 +0900

    ユーザーテーブルのテキストカラムバリデーション追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     root_path
   end
-  
+
   protected
   def configure_permitted_parameters
-    added_attrs = [ :name, :email, :password, :password_confirmation, :avatar ]
+    added_attrs = [ :name, :email, :password, :password_confirmation, :avatar, :text ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
