45a587f2904df5a95fd1d2208c1f20a8302f8ebc
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-01-06 17:56:53 +0900

    役職のバリデーション追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    added_attrs = [ :name, :email, :password, :password_confirmation, :avatar, :text ]
+    added_attrs = [ :name, :email, :password, :password_confirmation, :avatar, :text, :role ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
