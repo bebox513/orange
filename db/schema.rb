64c7c0a3c74c7a904cfdff4d7993658c0aa38a56
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-02-04 14:47:51 +0900

    Userテーブルにcompanyカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_065824) do
+ActiveRecord::Schema.define(version: 2021_01_28_015549) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_065824) do
     t.string "name"
     t.text "text"
     t.integer "role", null: false
+    t.string "company"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
