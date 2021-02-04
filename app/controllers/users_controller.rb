2604d82385e188e68c155253f048ef79c77a5dac
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-02-04 14:24:53 +0900

    フォロー一覧機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @followers = @user.followers
+    @followings = @user.followings
+    @button = params[:button]
   end
 end
