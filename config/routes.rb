90049dd5fb54b13ffd93ab5fb158d425986e0f15
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-02-06 10:37:03 +0900

    deviseのcontrollerを経由するようにルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: 'tops#index'
 
-  devise_for :users
-
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+   }
+   
   resources :users, :only => [:show] do
     resource :relationships, only: [:create, :destroy]
     get :follows, on: :member
