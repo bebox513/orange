430c4bddcf04a5516f54edec9a1834b12f2f4a28
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-02-11 14:46:40 +0900

    投稿をユーザー毎にまとめる変数を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @followers = @user.followers
     @followings = @user.followings
+    @posts = @user.posts.order(id: "DESC")
     @button = params[:button]
   end
 end
