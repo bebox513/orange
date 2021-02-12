1d9872d98a4df22216ad613d3a483cd47c403812
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-02-12 11:21:43 +0900

    comment,like,postのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,19 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => 'users/registrations'
    }
-   
+
   resources :users, :only => [:show] do
     resource :relationships, only: [:create, :destroy]
     get :follows, on: :member
     get :followers, on: :member
   end
 
+  resources :posts, :only => [:show, :new, :create] do
+    resource :likes, only: [:create, :destroy]
+    resource :comments, only: [:create, :destroy]
+  end
+
+  resources :comments, :only => [:new, :create, :destroy]
   resources :tops, :only => [:index, :show]
   resources :messages, :only => [:create]
   resources :rooms, :only => [:new, :create, :show, :index]
