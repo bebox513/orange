8071c2481570509a22088b82c35a09a17eed8827
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-02-12 11:23:59 +0900

    不要なマイグレーションファイル削除

--- db/migrate/20201019045959_add_name_to_corp_user.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToCorpUser < ActiveRecord::Migration[5.2]
-  def change
-    add_column :corp_users, :name, :string
-  end
-end
--- db/migrate/20201127084203_drop_table_corp_users.rb
+++
@@ -1,5 +0,0 @@
-class DropTableCorpUsers < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :corp_users
-  end
-end
