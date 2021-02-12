c9dca5270707c976c6b8a1cd3511749c2d190fb1
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-02-12 11:53:20 +0900

    Comment機能の追加

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @comment = Comment.find(prams[:id])
+    @comment.destroy
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :post_id)
+  end
+end
