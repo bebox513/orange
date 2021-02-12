b674840c33e64b2d4960d346f9d31d9e516c28b3
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-02-12 11:54:58 +0900

    Post機能の追加

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to "/posts/new"
+    else
+      redirect_to "/posts/new"
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :user_id)
+  end
+end
