a1aebd040b1ccf97576347940d0c5b5ee763ac2f
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-02-19 18:45:53 +0900

    ajaxの遷移方法の変更

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,7 +1,13 @@
 class TopsController < ApplicationController
   def index
-    @posts = Post.all.order(id: "DESC")
-    @button = params[:button]
+    @posts = Post.page(params[:page]).per(10)
+
+    return unless request.xhr?
+
+    case params[:type]
+    when 'posts', 'post', 'user'
+      render "tops/#{params[:type]}"
+    end
   end
 
   def show
