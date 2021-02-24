5682832d54c6e7da848ba0e6c83704930d065aa0
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-02-24 17:55:32 +0900

    アイコン編集のプレビュー機能追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,29 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).on("turbolinks:load", function () {
+  $("#user_avatar").on("change", function (e) {
+    var files = e.target.files;
+    var d = new $.Deferred().resolve();
+    $.each(files, function (i, file) {
+      d = d.then(function () {
+        return previewImage(file);
+      });
+    });
+  });
+
+  var previewImage = function (imageFile) {
+    var reader = new FileReader();
+    var img = new Image();
+    var def = $.Deferred();
+    reader.onload = function (e) {
+      $("#image_preview").empty();
+      $("#image_preview").append(img);
+      img.src = e.target.result;
+      def.resolve(img);
+    };
+    reader.readAsDataURL(imageFile);
+    return def.promise();
+  };
+});
