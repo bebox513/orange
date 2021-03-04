ad573ad1f7e161b3034c9f9aac600402a282c93f
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-03-04 12:27:42 +0900

    ルーティングにrooms indexを追加

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   resources :comments, :only => [:new, :create, :destroy]
   resources :tops, :only => [:index, :show]
   resources :messages, :only => [:create]
-  resources :rooms, :only => [:new, :create, :show, :index]
+  resources :rooms, :only => [:index, :new, :create, :show]
   resources :care_diaries, :only => [:index, :show, :new, :create, :edit, :update]
   resources :care_recipients, :only => [:index, :show, :new, :create, :edit, :update]
 end
