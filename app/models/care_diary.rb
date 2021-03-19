21e10bdaa41cecaf1d5e66ffb678666aef43ec29
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-03-19 17:45:32 +0900

    不要なコードを削除

--- app/models/care_diary.rb
+++ app/models/care_diary.rb
@@ -9,12 +9,4 @@ class CareDiary < ApplicationRecord
   validates :dinner_meal_intake, presence: true, numericality: { only_integer: true, less_than:11}
   validates :notes, presence: true,  length: { in: 1..255 }
   validates :user_id, presence: true
-
-  def cd_next
-    @next_date = self.next_month
-  end
-
-  def cd_back
-    @back_date = self.last_month
-  end
 end
