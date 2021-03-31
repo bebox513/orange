06ec09a9304f54afaa89b11b746d2f14d44f2100
高津成幸 <yosi@takatsuseikounoMacBook-Pro.local>
2021-03-31 12:45:27 +0900

    テストデータの追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,16 @@ FactoryBot.define do
     sequence(:email) { |n| "example#{n}@example.com" }
     password { "password" }
     password_confirmation { 'password' }
-    role {"1"}
+    role { "1" }
+  end
+end
+
+FactoryBot.define do
+  factory :user2, class: User do
+    name { "山田太郎" }
+    sequence(:email) { |n| "example#{n}@example.com" }
+    password { "password" }
+    password_confirmation { 'password' }
+    role { "2" }
   end
 end
